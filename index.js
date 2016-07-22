59e72df579867bc396e547a65a000479c0b612d2
zhishui <zhi_shui@hunliji.com>
2016-07-22 18:01:57 +0800

    1.1.2

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
+/*
+ react-es6-mixin
+ 支持传入3中形式的mixin:
+ 1:函数，函数必须带有函数名，@mixin(function test(){})
+ 2:对象，@mixin({key:value})
+ 3:数组,每个数组项必须是一个合法(符合1,2规则)的mixin
+ */
 (function(){
     'use strict';
-    var inject=function inject(mixinFun,component){
-        var mixinObj=mixinFun(component);
-        var keys=Object.keys(mixinObj);
-        var keysLen=keys.length;
-        for(var i=0;i<keysLen;i++){
-            var key=keys[i];
-            key==='getInitialState'?
-                component.state=mixinObj[key]():
-                component[key]=mixinObj[key];
+    function judgeTyle(obj){
+        return Object.prototype.toString.call(obj)
+    }
+    function combineFunMixin(mixinFun,component) {
+        var funName=mixinFun.name
+        if(funName===''){
+            throw new Error('确保你传入的mixin带有函数名')
+        }
+        component.prototype[funName]=mixinFun
+    }
+    function combineArrMixin(mixinArr,component) {
+        for(var i=0,len=mixinArr.length;i<len;i++){
+            mixin(mixinArr[i])(component)
         }
     }
-    var mixin=function minxin(component,mixins){
-        if(typeof mixins==='function'){
-            inject(mixins,component);
-        }else{
-            var count=mixins.length;
-            for(var i=0;i<count;i++){
-                inject(mixins[i],component);
+    function combineObjMixin(mixinObj,component){
+        Object.keys(mixinObj).forEach(function (key) {
+            component.prototype[key]=mixinObj[key]
+        })
+    }
+    function mixin(mixins){
+        return function (component) {
+            if(typeof mixins==='function'){
+                combineFunMixin(mixins,component)
+            }else if(judgeTyle(mixins)==='[object Array]'){
+                combineArrMixin(mixins,component)
+            }else if(judgeTyle(mixins)==='[object Object]'){
+                combineObjMixin(mixins,component)
+            }else{
+                throw new Error('确定你传入的mixin是一个函数或对象或数组')
             }
         }
     }
@@ -30,4 +49,4 @@
     } else {
         window.mixin = mixin;
     }
-}());
+}())
